ac8c6cc10f2290e88e58e79b5a54f43294c2133d
Dan Phillips <danmakenoise@gmail.com>
2016-02-28 23:51:31 -0500

    Add more table flips

--- lib/table_flipper.rb
+++ lib/table_flipper.rb
@@ -1,8 +1,16 @@
 class Exception
+  TABLE_FLIPS = [
+    "(╯°□°）╯︵ ┻━┻",
+    "‎(ﾉಥ益ಥ）ﾉ﻿ ┻━┻",
+    "(╯'□')╯︵ ┻━┻",
+    "┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻",
+    "/(ò.ó)┛彡┻━┻"
+  ]
+
   original_to_s = instance_method(:to_s)
 
   define_method :to_s do
   	original_message = original_to_s.bind(self).call
-  	"(╯°□°）╯︵ ┻━┻ : #{original_message}"
+  	"#{TABLE_FLIPS.sample} : #{original_message}"
   end
 end

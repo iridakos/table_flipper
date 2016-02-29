906039ab40065c3fedb17427808791301e0cd75e
Viktar Basharymau <6alliapumob@gmail.com>
2016-02-29 11:53:47 +0300

    Fix indentation
    Was: 1 tab, now: 2 spaces.

--- lib/table_flipper.rb
+++ lib/table_flipper.rb
@@ -2,7 +2,7 @@ class Exception
   original_to_s = instance_method(:to_s)
 
   define_method :to_s do
-  	original_message = original_to_s.bind(self).call
-  	"(╯°□°）╯︵ ┻━┻ : #{original_message}"
+    original_message = original_to_s.bind(self).call
+    "(╯°□°）╯︵ ┻━┻ : #{original_message}"
   end
 end
